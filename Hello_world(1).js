3bdfb211decf2aef59e4c2f572416b611c3eb6d8
Nabin <nabinbhat204@gmail.com>
2024-07-17 19:04:47 +0545

    Initial Commit

---
+++ Hello_world(1).js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+console.log("1"+"56");
+console.log(5+"56");
+console.log(56+'4');
+console.log(5-"56");
+console.log(56-'4');
+let a=35+"45",b=12+'34',c=22+33.333334,d=100000000000222222222222222,e=true,f=false,g=null,h=undefined;
+let i=Infinity,j=-Infinity,k=2023n,l=45-'67';
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+console.log(typeof h)
+console.log(typeof i)
+console.log(typeof j)
+console.log(typeof k)
+console.log(typeof l)
