4fc28837e8c739e781519ae3de157e4534d5e6de
Nabin <nabinbhat204@gmail.com>
2024-07-25 19:05:26 +0545

    Initial Commit

---
+++ var_and_let(2).js
@@ -0,0 +1,47 @@
+let a=35;
+{
+    let a=56;
+    let b=a*2;
+    console.log(a);
+}
+console.log(a);
+
+console.log("--------------------------------------------");
+
+var c="Nabin Bhat";
+{
+  var c="Pankaj Bhatt";
+  console.log(c);
+}
+console.log(c);
+
+console.log("--------------------------------------------");
+
+var d="hello";
+{
+    let d="world";
+    console.log(d);
+}
+console.log(d);
+
+var height;
+console.log(height);
+{
+  height=100;
+  var weight=60;
+  console.log(height);
+  console.log(weight);
+}
+console.log(height);
+console.log(weight);
+
+// let height;
+// console.log(height);
+// {
+//   height=100;
+//   let weight=60;
+//   console.log(height);
+//   console.log(weight);
+// }
+// console.log(height);
+// console.log(weight);
