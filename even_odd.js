3a34e332aceb6cd392a38dcdd8a81d38d7373ac8
Nabin <nabinbhat204@gmail.com>
2024-08-03 22:19:33 +0545

    Initial Commit

---
+++ even_odd.js
@@ -0,0 +1,10 @@
+const prompt=require('prompt-sync')();
+let n=prompt("Enter a number:");
+if(n%2==0)
+{
+    console.log(`${n} is an even number.`);
+}
+else
+{
+    console.log(`${n} is an odd number.`);
+}
