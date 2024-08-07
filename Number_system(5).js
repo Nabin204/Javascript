dc2e88de499009accaa8dfb038cc7a2a4f609a62
Nabin <nabinbhat204@gmail.com>
2024-08-07 20:01:28 +0545

    Initial Commit

---
+++ Number_system(5).js
@@ -0,0 +1,8 @@
+let a=1729;
+let b=0xabc; /* abc in hexadecimal=2748 in decimal */
+let c=0o123; /*123 in octal=83 in decimal */
+let d=0b110101011; /* 110101011 in binary=427 in decimal */
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
