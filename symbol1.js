beee15225ea2c40819ee76e579979c1e9cac88da
Nabin <nabinbhat204@gmail.com>
2024-08-10 08:56:25 +0545

    Initial Commit

---
+++ symbol1.js
@@ -0,0 +1,15 @@
+const student1 = Symbol("Nabin");
+const student2 = Symbol("Nabin");
+const student3 = Symbol("Mohit Singh Budal");
+console.log(student1 == student2);
+console.log(student1);
+console.log(student2);
+console.log(student3);
+
+console.log(student1.description);
+console.log(student2.description);
+console.log(student3.description);
+
+console.log(typeof student1);
+
+console.log(student1.description == student2.description);
