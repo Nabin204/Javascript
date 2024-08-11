c6c4e0aea96a0a259ff818c5c2a2ca166dd3a7d4
Nabin <nabinbhat204@gmail.com>
2024-08-11 21:13:40 +0545

    Initial Commit

---
+++ Sentences_inside_quotation_marks.js
@@ -0,0 +1,8 @@
+message1='He said,"How are you?"';
+message2="The girl answered her mother by saying,'I don't want to go alone.'";
+message3="He said,\"What are you doing.\"";
+message4='The girl answered her mother by saying,\'I do not want to go alone.\'';
+console.log(message1);
+console.log(message2);
+console.log(message3);
+console.log(message4);
