2929f030cfa6487e0e91c26c7e2159b344b67ea1
Nabin <nabinbhat204@gmail.com>
2024-08-13 08:26:46 +0545

    Initial Commit

---
+++ Spread_operator2.js
@@ -0,0 +1,12 @@
+const arr1= [1,2,3,4];
+const arr2= [5,6,7,8,9];
+let arr3=[...arr1,arr2]
+let arr4=[...arr1,...arr2];
+console.log(arr3);
+console.log(arr4);
+let arr5 = [...arr1];
+console.log(arr1);
+console.log(arr5);
+console.log(arr1.pop());
+console.log(arr1);
+console.log(arr5);
