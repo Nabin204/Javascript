6bdfd38eab0d3bebe06ae4d2fe727461a352585d
Nabin <nabinbhat204@gmail.com>
2024-08-16 19:54:02 +0545

    Initial Commit

---
+++ Hoisting(4).js
@@ -0,0 +1,17 @@
+// var height=150;
+// console.log(height);
+// console.log(weight); 
+// This line will cause an error because weight is not declared anywhere in the program
+
+var a=1729;
+console.log(a);
+console.log(b);
+var b;
+b=23;
+console.log(b);
+
+// var  height  =  180;
+// console.log(height);    //  ->  180
+// console.log(weight);    //  ->  undefined
+// var  weight  =  70;
+// console.log(weight);    //  ->  70
