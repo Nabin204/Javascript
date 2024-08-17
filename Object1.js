2f126fd682b699eb1fcc73c7914ca4e9eab9dfe9
Nabin <nabinbhat204@gmail.com>
2024-08-17 12:16:31 +0545

    Initial Commit

---
+++ Object1.js
@@ -0,0 +1,18 @@
+const obj={
+    Name : 'Nabin',
+    class: 'Bachelor second year',
+    roll_no: 20,
+    Adderss: 'Lamachaur',
+    marks: {
+        Mathematics : 78,
+        DL : 40,
+        ECT:48,
+        EDC:52,
+        TOC:51
+    }
+}
+console.log(obj.Adderss);
+console.log(obj.roll_no);
+console.log(obj.class);
+console.log(obj.marks);
+console.log(obj.marks.ECT);
