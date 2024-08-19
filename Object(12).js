c682bb673e49723d3f86dca78ad1d42c1aa9e787
Nabin <nabinbhat204@gmail.com>
2024-08-19 17:33:41 +0545

    Initial Commit

---
+++ Object(12).js
@@ -0,0 +1,20 @@
+let student1={
+    name:"Nabin Bhat",
+    age:21,
+    sex:"Male",
+    email:"nabinbhat204@gmail.com"
+};
+let student2={
+    name:"Bharat Thapa",
+    age:21,
+    sex:"Male",
+    email:"bharatthapa123@gmail.com"
+};
+console.log(student1.name);
+console.log(student1.age);
+console.log(student1.sex);
+console.log(student1.email);
+console.log(student2.name);
+console.log(student2.age);
+console.log(student2.sex);
+console.log(student2.email);
