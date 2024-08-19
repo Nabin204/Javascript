eda110120b3678410c98594468001eaecebe7a99
Nabin <nabinbhat204@gmail.com>
2024-08-19 17:32:15 +0545

    Initial Commit

---
+++ Array(8).js
@@ -0,0 +1,20 @@
+let a=["Nabin","Anil","Aryan","Aman","Keshav","Pankaj"];
+console.log(a);
+console.log(a[0]);
+console.log(a.length);
+a.push("Nayan");
+console.log(a[6]);
+console.log(a.length);
+a.sort();
+console.log(a);
+a.shift();
+console.log(a);
+a.unshift("Aman");
+console.log(a);
+
+let b=["Bhat","Giri","Dhami","Bhatta"];
+console.log(b.reverse());
+console.log(b);
+b.pop();
+console.log(b.pop());
+console.log(b);
