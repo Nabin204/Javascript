2322badcb6ceb851faeb620a2cd60b3e782c7078
Nabin <nabinbhat204@gmail.com>
2024-08-20 14:14:51 +0545

    Initial Commit

---
+++ Object(13).js
@@ -0,0 +1,14 @@
+let employee1={
+    Name:"Sujan Dotel",
+    Faculty:"Computer Engineering",
+}
+let employee2={
+    Name:"Subesh Ghimire",
+    Faculty:"Computer Engineering",
+}
+employee1.email="sujandotel456@gmail.com";
+employee1.age=20;
+console.log(employee1);
+console.log(employee2);
+delete employee1.email;
+console.log(employee1);
