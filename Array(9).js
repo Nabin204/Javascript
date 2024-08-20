ce63399757239d8d55b134de0a1d3bb59dead02d
Nabin <nabinbhat204@gmail.com>
2024-08-20 14:14:22 +0545

    Initial Commit

---
+++ Array(9).js
@@ -0,0 +1,13 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[4]="Pineapple";
+console.log(fruits);
+fruits[8]="Watermelon";
+console.log(fruits);
+console.log(fruits[5]);
+
+const days=["sun","mon","tue","wed","thr","fri","sat"];
+console.log(fruits.concat(days));
+console.log(fruits);
+console.log(days);
+days.pop();
+console.log(days);
