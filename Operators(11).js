e61fe41f74ac9c2d37959d1ce24746d68697ba57
Nabin <nabinbhat204@gmail.com>
2024-08-30 11:44:11 +0545

    Initial Commit

---
+++ Operators(11).js
@@ -0,0 +1,19 @@
+let a=1729,b=0,c="Nabin",d="";
+console.log(!a);
+console.log(!b);
+console.log(!c);
+console.log(!d);
+console.log(!!a);
+console.log(a && a);  //The 'AND operator' returns it's first operand if it evaluates to be false.
+console.log(a && b);
+console.log(a && c);
+console.log(a && d);
+console.log(a || a);   //if 'or operator' comes out to be true than the first operant is returned.
+console.log(a || b);
+console.log(a || c);
+console.log(a || d);
+console.log(4 || "nabin");
+console.log(4 && "nabin");
+console.log("nabin" || 4);
+console.log("nabin" && 4);
+
