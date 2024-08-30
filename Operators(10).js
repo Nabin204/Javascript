a161552b912f4233558ca51ed4d7072a7a00cba7
Nabin <nabinbhat204@gmail.com>
2024-08-30 11:43:51 +0545

    Initial Commit

---
+++ Operators(10).js
@@ -0,0 +1,45 @@
+"use strict"
+//Assignment operators
+let name1="Ram",name2;
+name2=name1;
+console.log(name2);
+
+//Arithmetic Operators
+let a=5,b=3;
+console.log("Multiplication:",a*b);
+console.log("Division:",a/b);
+console.log("Subtraction:",a-b);
+console.log("Addition:",a+b);
+console.log("Remainder:",a%b);
+console.log("Exponent:",a**b);
+
+//Unary arithmetic operator
+let n1=45,n2=32,n3=11,n4=23;
+console.log(n1++);
+console.log(n1);
+console.log(n2--);
+console.log(n2);
+console.log((n1++)+(n2++));
+console.log(n1);
+console.log(n2);
+console.log("**********************************************************************");
+console.log(++n3);
+console.log(n3);
+console.log(--n4);
+console.log(n4);
+console.log((++n3)+(++n4));
+console.log(n3);
+console.log(n4);
+
+//logical operators
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log(!false);
+console.log(!true);
+
